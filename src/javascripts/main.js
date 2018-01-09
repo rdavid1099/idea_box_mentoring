7869ed77bb221a9c5eb02cc744eb63c63d4a8e8f
Ryan Workman <rdavid1099@gmail.com>
2018-01-09 12:08:00 -0700

    Building out navbar

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,20 +1,30 @@
-const $ = require('jquery');
-
-let scope = {
-  username: 'Guest',
-  idea_num: 0
-}
+const $ = require('jquery')
 
 $(document).ready(() => {
-  $('#headline').html(
-    '<h1>IdeaBox</h1>' +
-    '<h4>IdeaBox template built by Ryan Workman</h4>' +
-    '<div>' +
-      '<p>Build out your code in the given files in ./src/.<br>' +
-      'Be sure to follow conventions and store all your CSS files in ./src/css/ and JS files in ./src/javascripts/.<br>' +
-      'For this simple application, you shouldn\'t have to install any additional npm modules, but if you wish, just run `npm install <package name>` and be sure to require it in ./src/javascripts/main.js</p>' +
-      '<p>To launch the local server, run `npm run build`. This will build the project and launch it on port 2020. Open a browser and go to http://localhost:2020</p>' +
-      '<p>Good luck and remember to have fun with it...<br>- Ryan<br>Github: <a href="https://github.com/rdavid1099" target="_blank">@rdavid1099</a></p>' +
-    '</div>'
-  )
+  let scope = {
+    username: 'Guest',
+    idea_num: 0,
+    loggedIn: false
+  }
+
+  function getNav() {
+    return '<nav class="navbar navbar-default">' +
+      '<div class="container-fluid">' +
+        '<div class="navbar-header"><span class="navbar-brand">IdeaBox</span></div>' +
+        '<ul class="nav navbar-nav navbar-right">' +
+          `<li><a href="#" id="login">${scope.loggedIn ? `Logout, ${scope.username}` : 'Login'}` +
+        '</ul>' +
+      '</div>' +
+    '</nav>'
+  }
+
+  function getHeadline() { return `<h2>Welcome to IdeaBox, ${scope.username}</h2>`}
+
+  $('#nav').html(getNav())
+  $('#headline').html(getHeadline())
+  $('#login').on('click', () => {
+    console.log(scope.loggedIn)
+    scope.loggedIn = !scope.loggedIn
+    $('#nav').html(getNav())
+  })
 })
