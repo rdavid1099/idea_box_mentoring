8fe6b5f1b735d6cfcce13be541d21bbe6d33ffd0
Ryan Workman <rdavid1099@gmail.com>
2018-01-09 11:10:12 -0700

    Build process is more durable

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -6,5 +6,15 @@ let scope = {
 }
 
 $(document).ready(() => {
-  $('#nav').html("<h1>IdeaBox</h1>")
+  $('#headline').html(
+    '<h1>IdeaBox</h1>' +
+    '<h4>IdeaBox template built by Ryan Workman</h4>' +
+    '<div>' +
+      '<p>Build out your code in the given files in ./src/.<br>' +
+      'Be sure to follow conventions and store all your CSS files in ./src/css/ and JS files in ./src/javascripts/.<br>' +
+      'For this simple application, you shouldn\'t have to install any additional npm modules, but if you wish, just run `npm install <package name>` and be sure to require it in ./src/javascripts/main.js</p>' +
+      '<p>To launch the local server, run `npm run build`. This will build the project and launch it on port 2020. Open a browser and go to http://localhost:2020</p>' +
+      '<p>Good luck and remember to have fun with it...<br>- Ryan<br>Github: <a href="https://github.com/rdavid1099" target="_blank">@rdavid1099</a></p>' +
+    '</div>'
+  )
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,32 @@
-var path = require('path');
+var path  = require('path'),
+    glob  = require('glob'),
+    shell = require('webpack-shell-plugin');
 
 var DIST_DIR   = path.join(__dirname, "dist"),
     CLIENT_DIR = path.join(__dirname, "src");
 
+function getAllJSFiles() {
+  var result   = [],
+      rawFiles = glob.sync("./src/javascripts/*.js");
+  for (var i = 0; i < rawFiles.length; i++) { result.push(rawFiles[i].replace("/src", "").replace(/\.[^/.]+$/, "")) }
+  return result;
+}
+
 module.exports = {
   context: CLIENT_DIR,
-  entry: "./javascripts/main",
+  entry: getAllJSFiles(),
   output: {
     path: DIST_DIR,
     filename: "bundle.js"
   },
   resolve: {
     extensions: ['.js']
-  }
+  },
+  plugins: [
+    new shell({
+      onBuildStart: ['echo "Starting build"'],
+      onBuildEnd: ['npm run copy']
+    })
+  ],
+  watch: true
 }
